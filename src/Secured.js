0eb5bcaf2f3b707bc4b5a1994568dda4e5b05587
PavelNestor <pavelnesterets82@gmail.com>
2019-11-04 18:28:09 +0200

    init

--- src/Secured.js
+++ src/Secured.js
@@ -12,7 +12,7 @@ class Secured extends Component {
 
     componentDidMount() {
         const keycloak = Keycloak('/keycloak.json');
-        keycloak.init({onLoad: 'login-required'}).then(authenticated => {
+        keycloak.init({onLoad: 'login-required'}).success(authenticated => {
             this.setState({ keycloak: keycloak, authenticated: authenticated })
         })
     }
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -9,7 +9,7 @@ class UserInfo extends Component {
             email: "",
             id: ""
         };
-        this.props.keycloak.loadUserInfo().then(userInfo => {
+        this.props.keycloak.loadUserInfo().success(userInfo => {
             this.setState({name: userInfo.name, email: userInfo.email, id: userInfo.sub})
         });
     }
